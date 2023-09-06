5c895731daf945b3ea6dd34051889b2c8e7192fa
Ball Panuwat <79345283+ballpanuwat25@users.noreply.github.com>
2023-09-06 19:32:11 +0700

    Update db.js

--- config/db.js
+++ config/db.js
@@ -2,7 +2,19 @@ import { Sequelize } from "sequelize";
 
 const db = new Sequelize('specialproject', '472xknavxg70ozh51zee', 'pscale_pw_qn6Wj8TOyfkeMdImKnu7F7mZ6myxx0m5tBzyYq8COpl',{
     host: 'aws.connect.psdb.cloud',
-    dialect: 'mysql'
+    dialect: 'mysql',
+    dialectOptions: {
+        ssl: 'Amazon RDS', 
+    },
 });
+
+sequelize
+  .authenticate()
+  .then(() => {
+    console.log('Connection has been established successfully.');
+  })
+  .catch((err) => {
+    console.error('Unable to connect to the database:', err);
+  });
 
 export default db;
