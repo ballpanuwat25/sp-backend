f75c3c2be25461708849d06a47b2a14fe8292fb5
Ball Panuwat <79345283+ballpanuwat25@users.noreply.github.com>
2023-11-13 09:05:48 +0700

    Update 13.11.2023

--- index.js
+++ index.js
@@ -2,29 +2,29 @@ import cors from 'cors';
 import express from 'express';
 import cookieParser from 'cookie-parser';
 
-import AdminRoute from './admin/AdminRoute.js';
-import AdminCrudRoute from './admin/adminCrud/routes/AdminCrudRoute.js';
-import StaffCrudRoute from './admin/staffCrud/routes/StaffCrudRoute.js';
-import TeacherCrudRoute from './admin/teacherCrud/routes/TeacherCrudRoute.js';
+import AdminRoute from './src/admin/AdminRoute.js';
+import AdminCrudRoute from './src/admin/adminCrud/routes/AdminCrudRoute.js';
+import StaffCrudRoute from './src/admin/staffCrud/routes/StaffCrudRoute.js';
+import TeacherCrudRoute from './src/admin/teacherCrud/routes/TeacherCrudRoute.js';
 
-import ChemicalsRoute from './staff/chemicals/routes/ChemicalsRoute.js';
-import ChemicalsDetailRoute from './staff/chemicalsDetails/routes/ChemicalsDetailRoute.js';
+import ChemicalsRoute from './src/staff/chemicals/routes/ChemicalsRoute.js';
+import ChemicalsDetailRoute from './src/staff/chemicalsDetails/routes/ChemicalsDetailRoute.js';
 
-import EquipmentRoute from './staff/equipment/routes/EquipmentRoute.js';
-import EquipmentCategoryRoute from './staff/equipmentCategory/routes/EquipmentCategoryRoute.js';
+import EquipmentRoute from './src/staff/equipment/routes/EquipmentRoute.js';
+import EquipmentCategoryRoute from './src/staff/equipmentCategory/routes/EquipmentCategoryRoute.js';
 
-import StudentRoute from './student/StudentRoute.js'
-import ApproveStudentRoute from './staff/approveStudent/routes/ApproveStudentRoute.js'
+import StudentRoute from './src/student/StudentRoute.js'
+import ApproveStudentRoute from './src/staff/approveStudent/routes/ApproveStudentRoute.js'
 
-import ChemicalsRequestRoute from './student/studentChemicalsRequest/routes/ChemicalsRequestRoute.js';
-import EquipmentRequestRoute from './student/studentEquipmentRequest/routes/EquipmentRequestRoute.js';
+import ChemicalsRequestRoute from './src/student/studentChemicalsRequest/routes/ChemicalsRequestRoute.js';
+import EquipmentRequestRoute from './src/student/studentEquipmentRequest/routes/EquipmentRequestRoute.js';
 
-import LogActivityRoute from './admin/logActivity/routes/LogActivityRoute.js';
+import LogActivityRoute from './src/admin/logActivity/routes/LogActivityRoute.js';
 
-import BundleRequestRoute from './teacher/bundle/routes/BundleRequestRoute.js';
+import BundleRequestRoute from './src/teacher/bundle/routes/BundleRequestRoute.js';
 
-import TeacherRoute from './teacher/TeacherRoute.js'
-import StaffRoute from './staff/StaffRoute.js'
+import TeacherRoute from './src/teacher/TeacherRoute.js'
+import StaffRoute from './src/staff/StaffRoute.js'
 
 const app = express();
 const port = process.env.PORT || 3001;
